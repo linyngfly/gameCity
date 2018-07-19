b3d2df237b71ce7df768161616368ce6f3ff697d
linyng <linyngfly@163.com>
2018-07-19 10:02:09 +0800

    项目初始化体检

---
+++ client-h5/bin/js/LayaSample.js
@@ -0,0 +1,10 @@
+var WebGL = Laya.WebGL;
+// 程序入口
+var GameMain = /** @class */ (function () {
+    function GameMain() {
+        Laya.init(2560, 1440, WebGL);
+    }
+    return GameMain;
+}());
+new GameMain();
+//# sourceMappingURL=LayaSample.js.map
---
+++ client-h5/src/LayaSample.ts
@@ -0,0 +1,9 @@
+import WebGL = Laya.WebGL;
+// 程序入口
+class GameMain{
+    constructor()
+    {
+        Laya.init(1334,750, WebGL);
+    }
+}
+new GameMain();
